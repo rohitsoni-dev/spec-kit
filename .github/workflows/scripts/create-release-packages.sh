52bbf82f77569e49fefb8ca3909e40592186746a
rohit <rohitsoni259@gmail.com>
2025-10-22 11:39:12 +0530

    Refactor YAML frontmatter insertion into generic function for continue agent support

--- .github/workflows/scripts/create-release-packages.sh
+++ .github/workflows/scripts/create-release-packages.sh
@@ -94,31 +94,7 @@ generate_commands() {
       # Find description in body and write invokable: true in next line
       if [[ -n "$description" ]]; then
         # Insert name and invokable fields after the first --- delimiter
-        body=$(printf '%s' "$body" | awk -v name="$name" '
-        BEGIN { in_frontmatter = 0; processed = 0 }
-        /^---$/ { 
-          print
-          if (!processed) {
-            in_frontmatter = 1
-          }
-          next
-        }
-        in_frontmatter && /^description:/ && !processed { 
-          print "name: speckit." name
-          print
-          print "invokable: true"
-          processed = 1
-          next
-        }
-        in_frontmatter && /^[a-zA-Z]/ && !processed {
-          print "name: speckit." name
-          print "invokable: true"
-          print
-          processed = 1
-          in_frontmatter = 0
-        }
-        !in_frontmatter || processed { print }
-        ')  
+        body=$(insert_yaml_frontmatter "$body" "$name" "invokable" "true")
       fi
     fi
 
@@ -132,6 +108,40 @@ generate_commands() {
         echo "$body" > "$output_dir/speckit.$name.$ext" ;;
     esac
   done
+}
+
+# Generic function to insert custom YAML frontmatter into prompt files
+insert_yaml_frontmatter() {
+  local body=$1
+  local name=$2
+  local field_name=$3
+  local field_value=$4
+  
+  printf '%s' "$body" | awk -v name="$name" -v field="$field_name" -v value="$field_value" '
+    BEGIN { in_frontmatter = 0; processed = 0 }
+    /^---$/ { 
+      print
+      if (!processed) {
+        in_frontmatter = 1
+      }
+      next
+    }
+    in_frontmatter && /^description:/ && !processed { 
+      print "name: speckit." name
+      print
+      print field ": " value
+      processed = 1
+      next
+    }
+    in_frontmatter && /^[a-zA-Z]/ && !processed {
+      print "name: speckit." name
+      print field ": " value
+      print
+      processed = 1
+      in_frontmatter = 0
+    }
+    !in_frontmatter || processed { print }
+  '
 }
 
 build_variant() {
